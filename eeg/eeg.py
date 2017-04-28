f6651990e3254b1f53d1146b7a09472736ab7c0b
Corazon <tdyshin@gmail.com>
2017-04-27 20:26:43 -0400

    Change project directory structure

--- eeg/eeg.py
+++ eeg/eeg.py
@@ -10,8 +10,14 @@ app.config.from_object(__name__) # load config from this file , flaskr.py
 # Load default config and override config from an environment variable
 app.config.update(dict(
     DATABASE=os.path.join(app.root_path, 'eeg.db'),
-    SECRET_KEY='development key',
+    SECRET_KEY='c2NQEROndfk23KlO3',
     USERNAME='admin',
     PASSWORD='default'
 ))
 app.config.from_envvar('EEG_SETTINGS', silent=True)
+
+def connect_db():
+    """Connects to the specific database."""
+    rv = sqlite3.connect(app.config['DATABASE'])
+    rv.row_factory = sqlite3.Row
+    return rv
